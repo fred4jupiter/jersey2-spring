87b21d98472c12558c0df45626798711bfe66346
Staehler, Michael <michael.staehler@opitz-consulting.com>
2014-07-01 15:45:47 +0200

    updated async example

--- src/test/java/de/fred4jupiter/jerseyspring/rest/AsyncResourceTest.java
+++ src/test/java/de/fred4jupiter/jerseyspring/rest/AsyncResourceTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.ws.rs.client.InvocationCallback;
 import javax.ws.rs.core.Response;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -18,6 +19,30 @@ public class AsyncResourceTest extends AbstractJerseyTest {
     @Test
     public void callAsyncMethod() throws ExecutionException, InterruptedException {
         Future<Response> asyncResponse = target("async").request().async().get();
+        assertNotNull(asyncResponse);
+        Response response = asyncResponse.get();
+        assertNotNull(response);
+        String bodyResponse = response.readEntity(String.class);
+        LOG.debug("bodyResponse=" + bodyResponse);
+        assertThat(bodyResponse, equalTo("HELLO"));
+    }
+
+    @Test
+    public void asyncResourceCallWithCallbackHandler() throws ExecutionException, InterruptedException {
+        final Future<Response> asyncResponse = target("async")
+                .request().async().get(new InvocationCallback<Response>() {
+                    @Override
+                    public void completed(Response response) {
+                        System.out.println("Response status code " + response.getStatus() + " received.");
+                    }
+
+                    @Override
+                    public void failed(Throwable throwable) {
+                        System.out.println("Invocation failed.");
+                        throwable.printStackTrace();
+                    }
+                });
+
         assertNotNull(asyncResponse);
         Response response = asyncResponse.get();
         assertNotNull(response);
